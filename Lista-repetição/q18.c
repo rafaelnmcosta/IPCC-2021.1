6b5ccd8240bd7f080fd39e4201c74ba1414e2761
rafaelnmcosta <rafaelnmcosta@outlook.com>
2021-09-24 21:20:30 -0300

    teste

---
+++ Lista-repetição/q18.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+    double valorIngresso, valorInicial, valorFinal;
+
+    scanf("%lf %lf %lf", &valorIngresso, &valorInicial, &valorFinal);
+
+    if(valorInicial>=valorFinal){
+        printf("INTERVALO INVALIDO\n");
+        return 0;
+    }
+
+
+
+    
+
+    return 0;
+}
